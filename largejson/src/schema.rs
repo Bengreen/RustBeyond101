e0e8dd31cf7e3c912e22c4288b6ec206ec963ae3
Ben Greene <BenJGreene@gmail.com>
2023-10-08 17:10:13 +0100

    Testing

--- largejson/src/schema.rs
+++ largejson/src/schema.rs
@@ -7,6 +7,8 @@ use serde_json::Value;
 
 use crate::error::MyError;
 
+
+
 // Describe a person using a simple data structure
 #[derive(JsonSchema, Serialize, Deserialize)]
 pub struct Person {
@@ -43,6 +45,19 @@ pub fn schema_person_string() -> Result<String, MyError> {
 ///
 /// Can use this to generate the JsonSchema for any [Serialize] object rather than writing one function for each object.
 /// This function can be used instead of [schema_person_string]
+///
+/// ```
+/// # use largejson::error::MyError;
+/// # use largejson::schema::Person;
+/// # use largejson::schema::schema_string;
+/// # fn main() -> Result<(), MyError> {
+/// let person_schema = schema_string::<Person>()?;
+///
+/// assert!(person_schema.len() > 0);
+/// println!("Schema = {}", person_schema);
+/// # Ok(())
+/// # }
+/// ```
 pub fn schema_string<MyType: JsonSchema>() -> Result<String, MyError> {
     let my_schema = schema_for!(MyType);
     let my_schema_json = serde_json::to_string_pretty(&my_schema)?;
@@ -92,4 +107,34 @@ pub fn validate<R: Read>(schema: &str, reader: R, error_limit: usize) -> Result<
     }
 
     Ok(data_value)
+}
+
+
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn validate_vec() {
+
+        let schema = schema_string::<Vec<Person>>().expect("got schema");
+
+        println!("my schema is {schema}");
+
+        let error_limit = 2;
+
+        let size = 220;
+
+        let my_data_vec: Vec<_> = (0..size).map(|x| Person{ name: format!("name-{:08}", x), age: x, }).collect();
+        let my_data_string = serde_json::to_string_pretty(&my_data_vec).expect("to string");
+        let my_data_bytes = my_data_string.as_bytes();
+        let validated = validate(&schema, my_data_bytes, error_limit).expect("validated");
+
+        let validated_typed: Vec<Person> = serde_json::from_value(validated).expect("conversion to Explicit Type");
+        assert!(validated_typed.len() == usize::try_from(size).unwrap());
+
+        println!("There are {} records", validated_typed.len());
+    }
 }
